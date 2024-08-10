026e036f4d10aa04bed022bd623c665818983e7f
Tayyeb butt <tayyebbutt966@gmail.com>
2024-08-10 14:30:22 +0500

    antd added

---
+++ components/header/navigation-nav/megamenu.jsx
@@ -0,0 +1,77 @@
+"use client";
+import React from "react";
+
+export default function MegaMenu() {
+    return (
+        <>
+   
+        {/* <div className="absolute top-full mt-2 w-screen bg-white shadow-lg z-50">
+            <div className="px-10 py-6 max-w-screen-xl mx-auto">
+                <div className="grid grid-cols-6 gap-8">
+                    <div>
+                        <h4 className="font-bold mb-2">Featured Brands</h4>
+                        <ul className="space-y-1">
+                            <li>7th Heaven</li>
+                            <li>Biotherm</li>
+                            <li>Bobbi Brown</li>
+                            <li>Clarins</li>
+                            <li>Clinique</li>
+                            <li>Delia</li>
+                            <li>Dr. Hauschka</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h4 className="font-bold mb-2">Moisturisers</h4>
+                        <ul className="space-y-1">
+                            <li>Balm</li>
+                            <li>Lotion</li>
+                            <li>Day Cream</li>
+                            <li>Night Cream</li>
+                            <li>Face Oil</li>
+                            <li>Face Serum</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h4 className="font-bold mb-2">Eye Care</h4>
+                        <ul className="space-y-1">
+                            <li>Eye Serum</li>
+                            <li>Eye Cream</li>
+                            <li>Eye Gel</li>
+                            <li>Eye Patch</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h4 className="font-bold mb-2">Cleansers</h4>
+                        <ul className="space-y-1">
+                            <li>Face Wash & Cleanser</li>
+                            <li>Liquid Exfoliators</li>
+                            <li>Face Scrubs</li>
+                            <li>Micellar Water</li>
+                            <li>Wipes</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h4 className="font-bold mb-2">Toners</h4>
+                        <ul className="space-y-1">
+                            <li>Facial Spray</li>
+                            <li>Toner</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h4 className="font-bold mb-2">Masks</h4>
+                        <ul className="space-y-1">
+                            <li>Face Mask</li>
+                            <li>Sheet Mask</li>
+                            <li>Clay Mask</li>
+                            <li>Peel Off Mask</li>
+                            <li>Eye Mask</li>
+                            <li>Lip Mask</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div> */}
+   
+        </>     
+    );
+}
