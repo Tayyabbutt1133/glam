b83283dcb51b377c540cbb52b3515ed93437d881
Uzain Bin Mubarak <81316099+uzainmubarak@users.noreply.github.com>
2024-08-13 13:46:25 +0500

    font size changed

--- components/header/navigation-nav/navigation.jsx
+++ components/header/navigation-nav/navigation.jsx
@@ -25,7 +25,7 @@ export default function Navigation() {
   return (
     <>
       <div
-        className={`${jost.className} flex items-center h-[66px] font-normal lg:text-base xl:text-xl bg-white`}
+        className={`${jost.className} flex items-center h-[66px] font-normal lg:text-sm xl:text-xl bg-white`}
       >
         <Container>
           <nav className="flex flex-row w-full justify-center items-center py-3 lg:gap-4 xl:gap-6 2xl:gap-11">
