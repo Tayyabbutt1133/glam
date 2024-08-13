cea2710d35a4c97b5f1306be22cd49cee463c0e0
Uzain Bin Mubarak <81316099+uzainmubarak@users.noreply.github.com>
2024-08-13 08:28:34 +0500

    hydration error resolved & arrow & cart icon added

--- components/header/news-banner-nav/news-banner-nav.jsx
+++ components/header/news-banner-nav/news-banner-nav.jsx
@@ -1,102 +1,123 @@
 "use client";
+import { useEffect, useState } from "react";
 import Image from "next/image";
-import { IoIosArrowDown } from "react-icons/io";
+import ArrowDown from "../../../public/icons/arrow-down";
+import { Lexend_Deca } from "next/font/google";
+
 import Container from "../../container";
 import { usePopupStore } from "../../../states/use-popup-store.jsx";
-import { useEffect, useState } from "react";
+
+const lexendDeca = Lexend_Deca({ subsets: ["latin"] });
 
 export default function NewsBannerNav() {
-    const [mounted, setMounted] = useState(false);
-    const onOpen = usePopupStore((state) => state.onOpen);
-    const selectedCountryFromStore = usePopupStore((state) => state.selectedCountry);
-    
+  const [mounted, setMounted] = useState(false);
+  const onOpen = usePopupStore((state) => state.onOpen);
+  const selectedCountryFromStore = usePopupStore(
+    (state) => state.selectedCountry
+  );
 
-    const [selectedCountry, setSelectedCountry] = useState(() => {
-        // Check if window is defined to ensure code runs in the browser
+  const [selectedCountry, setSelectedCountry] = useState(() => {
+    // Check if window is defined to ensure code runs in the browser
+    if (typeof window !== "undefined") {
+      if (selectedCountryFromStore && selectedCountryFromStore.countryCode) {
+        return selectedCountryFromStore;
+      }
+      const savedCountry = localStorage.getItem("selectedCountry");
+      return savedCountry
+        ? JSON.parse(savedCountry)
+        : { countryCode: "", code: "", country: "" };
+    } else {
+      return { countryCode: "", code: "", country: "" }; // Default value during SSR
+    }
+  });
+
+  const [flagUrl, setFlagUrl] = useState("");
+
+  const fetchFlag = async (countryCode) => {
+    try {
+      const response = await fetch(
+        `https://restcountries.com/v3.1/alpha/${countryCode}`
+      );
+      const data = await response.json();
+      setFlagUrl(data[0].flags.svg);
+    } catch (error) {
+      console.error("Error fetching flag:", error);
+    }
+  };
+
+  useEffect(() => {
+    if (selectedCountry.countryCode) {
+      fetchFlag(selectedCountry.countryCode);
+    }
+  }, [selectedCountry]);
+
+  useEffect(() => {
+    // Sync state with the store when the popup selection changes
+    const unsubscribe = usePopupStore.subscribe((state) => {
+      const updatedCountry = state.selectedCountry;
+      if (
+        updatedCountry &&
+        updatedCountry.countryCode !== selectedCountry.countryCode
+      ) {
+        setSelectedCountry(updatedCountry);
         if (typeof window !== "undefined") {
-            if (selectedCountryFromStore && selectedCountryFromStore.countryCode) {
-                return selectedCountryFromStore;
-            }
-            const savedCountry = localStorage.getItem('selectedCountry');
-            return savedCountry ? JSON.parse(savedCountry) : { countryCode: '', code: '', country: '' };
-        } else {
-            return { countryCode: '', code: '', country: '' }; // Default value during SSR
+          localStorage.setItem(
+            "selectedCountry",
+            JSON.stringify(updatedCountry)
+          );
         }
+      }
     });
 
-    const [flagUrl, setFlagUrl] = useState('');
+    return () => unsubscribe();
+  }, [selectedCountry]);
 
-    const fetchFlag = async (countryCode) => {
-        try {
-            const response = await fetch(`https://restcountries.com/v3.1/alpha/${countryCode}`);
-            const data = await response.json();
-            setFlagUrl(data[0].flags.svg);
-        } catch (error) {
-            console.error("Error fetching flag:", error);
-        }
-    };
+  // Set mounted to true after the component mounts
+  // This is to prevent the component from rendering on the server
+  // and causing a hydration mismatch
+  useEffect(() => {
+    setMounted(true);
+  }, []);
 
-    useEffect(() => {
-        if (selectedCountry.countryCode) {
-            fetchFlag(selectedCountry.countryCode);
-        }
-    }, [selectedCountry]);
+  if (!mounted) {
+    return null;
+  }
 
-    useEffect(() => {
-        // Sync state with the store when the popup selection changes
-        const unsubscribe = usePopupStore.subscribe((state) => {
-            const updatedCountry = state.selectedCountry;
-            if (updatedCountry && updatedCountry.countryCode !== selectedCountry.countryCode) {
-                setSelectedCountry(updatedCountry);
-                if (typeof window !== "undefined") {
-                    localStorage.setItem('selectedCountry', JSON.stringify(updatedCountry));
-                }
-            }
-        });
-
-        return () => unsubscribe();
-    }, [selectedCountry]);
-
-    // Set mounted to true after the component mounts
-    // This is to prevent the component from rendering on the server
-    // and causing a hydration mismatch
-    useEffect(() => {
-        setMounted(true);
-      }, []);
-
-      if (!mounted) {
-        return null;
-      }
-
-    return (
-        <div className="w-full bg-[#F7EBE0]">
-            <Container>
-                <div className="flex w-full justify-center items-center relative py-2">
-                    <p className="text-center">
-                        Up to 50% off selected brands + UK next day delivery over £40
-                    </p>
-                    <div className="flex flex-row gap-1 absolute right-0">
-                        <div className="relative flex w-10">
-                            {flagUrl && (
-                                <Image 
-                                    src={flagUrl} 
-                                    alt={`${selectedCountry.country} Flag`} 
-                                    width={31}
-                                    height={22}
-                                    objectFit="contain"
-                                />
-                            )}
-                        </div>
-                        <button 
-                            className="flex flex-row items-center cursor-pointer"
-                            onClick={onOpen}
-                        >
-                            <span>{`${selectedCountry.countryCode} - ${selectedCountry.code}`}</span>
-                            <IoIosArrowDown color="black"/>
-                        </button>
-                    </div>
+  return (
+    <div className={`${lexendDeca.className} w-full bg-[#F7EBE0]`}>
+      <Container>
+        <div className="flex w-full justify-center items-center relative py-2">
+          <p className="text-center text-base font-medium">
+            Up to 50% off selected brands + UK next day delivery over £40
+          </p>
+          <div className="flex flex-row gap-1 absolute right-0">
+            <div className="relative flex w-10">
+              {flagUrl && (
+                <Image
+                  src={flagUrl}
+                  alt={`${selectedCountry.country} Flag`}
+                  width={31}
+                  height={22}
+                  objectFit="contain"
+                />
+              )}
+            </div>
+            <button
+              className="flex flex-row items-center cursor-pointer gap-2"
+              onClick={onOpen}
+            >
+                <div className="font-normal text-base">
+                    <span className="lowercase">
+                        {selectedCountry.countryCode}
+                    </span>
+                     {" "} - {selectedCountry.code}
                 </div>
-            </Container>
+              
+              <ArrowDown className="w-3 h-auto" />
+            </button>
+          </div>
         </div>
-    );
+      </Container>
+    </div>
+  );
 }
