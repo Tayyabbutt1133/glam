879e265e5b438702a400b1680daac73ec6bbde2f
Uzain Bin Mubarak <uzainmubarak@gmail.com>
2024-08-17 16:02:28 +0500

    Slider fully working

--- components/home/hero/hero.jsx
+++ components/home/hero/hero.jsx
@@ -1,17 +1,22 @@
 "use client";
 import Image from "next/image";
-// import Slider from "react-slick";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import mob_image_path from "../../../public/hero-banners/mob-banner.png"
-import next_icon from "../../../public/hero-banners/next-arrows.svg"
+
+import mob_image_path from "/public/hero-banners/mob-banner.png";
+import NextArrowIcon from "/public/hero-banners/next-arrow";
+import PrevArrowIcon from "/public/hero-banners/prev-arrow";
+import Banner1 from "/public/hero-banners/Section1.png";
+import Banner2 from "/public/hero-banners/Banner2.png";
 
 import useMediaQuery from "../../../hooks/useMediaQuery";
 
-const img = [
-  { src: "/public/hero-banners/Section1.png" },
-  { src: "/public/hero-banners/Banner2.png" },
+const images = [
+  { src: Banner1 },
+  { src: Banner1 },
+  { src: Banner2 },
+  { src: Banner2 },
 ];
 
 const mobImages = [
@@ -20,44 +25,44 @@ const mobImages = [
   { src: mob_image_path },
 ];
 
-const NextArrow = ({ className, style, onClick }) => (
-  <div className={`arrow ${className}`} onClick={onClick}>
-    {/* <Image
-      src={next_icon}
-      width={50}
-      height={50}
-      alt="Next"
-      style={{ display: "block"}}
-    /> */}
-    <div style={{display: "block", }}>
-
-    Next
+const NextArrow = ({ className, style, onClick }) => {
+  return (
+    <div
+      className={`arrow ${className}`}
+      onClick={onClick}
+      style={{ ...style, right: "80px" }}
+    >
+      <NextArrowIcon />
     </div>
-  </div>
-);
+  );
+};
 
-const PrevArrow = ({ className, style, onClick }) => (
-  <div className={className} onClick={onClick} >
-    {/* <Image
-      src="/public/hero-banners/prev-arrows.svg"
-      width={50}
-      height={50}
-      alt="Previous"
-    /> */}
-    Previous
-  </div>
-);
+const PrevArrow = ({ className, style, onClick }) => {
+  return (
+    <div
+      className={`arrow ${className}`}
+      onClick={onClick}
+      style={{ ...style, left: "20px", zIndex: 1 }}
+    >
+      <PrevArrowIcon />
+    </div>
+  );
+};
 
 const Banner = ({ src }) => {
   return (
-    <div className="flex flex-row max-w-screen-lg">
-      <Image src={src} width={500} height={500} alt="banner" />
+    <div className="flex max-w-screen-[1920px]">
+      <Image
+        src={src}
+        style={{ width: "100%", height: "auto", objectFit: "contain" }}
+        alt="banner"
+      />
     </div>
   );
 };
 
 export default function Hero() {
-  const isMobile = useMediaQuery("(max-width: 768px)");
+  const isMobile = useMediaQuery("(max-width: 1024px)");
   const settings = {
     autoplay: true,
     dots: isMobile,
@@ -71,35 +76,27 @@ export default function Hero() {
     slidesToScroll: 1,
     arrows: !isMobile, // Show arrows only on non-mobile devices
     swipe: isMobile, // Enable swipe on mobile devices
-    nextArrow: <NextArrow />,
-    prevArrow: <PrevArrow />,
+    nextArrow: <NextArrow to="next" />,
+    prevArrow: <PrevArrow to="prev" />,
   };
 
   return (
     <>
       {/* For mobile  */}
-      <div className="mb-10 xl:hidden">
- 
+      <div className="mb-10 lg:hidden">
         <Slider {...settings}>
           {mobImages.map((img, idx) => (
-                <Banner key={idx} src={img.src} />
-            ))}
+            <Banner key={idx} src={img.src} />
+          ))}
         </Slider>
-
       </div>
 
       {/* For large screens */}
-      <div className="hidden xl:block">
+      <div className="hidden lg:block">
         <Slider {...settings}>
-        <div>
-          <h3>1</h3>
-        </div>
-        <div>
-          <h3>2</h3>
-        </div>
-        <div>
-          <h3>3</h3>
-        </div>
+          {images.map((img, idx) => (
+            <Banner key={idx} src={img.src} />
+          ))}
         </Slider>
       </div>
     </>
---
+++ public/hero-banners/next-arrow.jsx
@@ -0,0 +1,65 @@
+export default function NextArrowIcon({ className }) {
+  return (
+    <svg
+      width="64"
+      height="65"
+      viewBox="0 0 64 65"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+    >
+      <g filter="url(#filter0_d_3_21578)">
+        <path
+          d="M32 56.5C47.464 56.5 60 43.964 60 28.5C60 13.036 47.464 0.5 32 0.5C16.536 0.5 4 13.036 4 28.5C4 43.964 16.536 56.5 32 56.5Z"
+          fill="white"
+        />
+        <path
+          d="M32 56C47.1878 56 59.5 43.6878 59.5 28.5C59.5 13.3122 47.1878 1 32 1C16.8122 1 4.5 13.3122 4.5 28.5C4.5 43.6878 16.8122 56 32 56Z"
+          stroke="#001325"
+          stroke-opacity="0.08"
+        />
+        <path
+          d="M29.2499 35C29.0509 35.0022 28.8557 34.9447 28.6896 34.8351C28.5234 34.7255 28.3938 34.5686 28.3175 34.3848C28.2412 34.2009 28.2216 33.9984 28.2612 33.8034C28.3008 33.6083 28.3979 33.4295 28.5399 33.29L33.3399 28.5L28.5399 23.71C28.4467 23.6142 28.3733 23.5009 28.3238 23.3766C28.2743 23.2524 28.2499 23.1196 28.2517 22.9859C28.2536 22.8522 28.2817 22.7201 28.3346 22.5973C28.3875 22.4745 28.4641 22.3633 28.5599 22.27C28.6558 22.1768 28.7691 22.1033 28.8933 22.0539C29.0176 22.0044 29.1504 21.9799 29.2841 21.9818C29.4178 21.9836 29.5498 22.0118 29.6727 22.0647C29.7955 22.1176 29.9067 22.1942 29.9999 22.29L35.4999 27.79C35.5934 27.8831 35.6676 27.9938 35.7182 28.1156C35.7688 28.2374 35.7949 28.3681 35.7949 28.5C35.7949 28.632 35.7688 28.7626 35.7182 28.8844C35.6676 29.0063 35.5934 29.1169 35.4999 29.21L29.9999 34.71C29.9018 34.8076 29.7845 34.8838 29.6554 34.9337C29.5263 34.9836 29.3882 35.0062 29.2499 35Z"
+          fill="#202020"
+        />
+      </g>
+      <defs>
+        <filter
+          id="filter0_d_3_21578"
+          x="0"
+          y="0.5"
+          width="64"
+          height="64"
+          filterUnits="userSpaceOnUse"
+          color-interpolation-filters="sRGB"
+        >
+          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy="4" />
+          <feGaussianBlur stdDeviation="2" />
+          <feComposite in2="hardAlpha" operator="out" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="BackgroundImageFix"
+            result="effect1_dropShadow_3_21578"
+          />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="effect1_dropShadow_3_21578"
+            result="shape"
+          />
+        </filter>
+      </defs>
+    </svg>
+  );
+}
