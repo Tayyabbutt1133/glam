8912f797d7f558e4610f4e7d3d2e14a71ee192d5
Uzain Bin Mubarak <uzainmubarak@gmail.com>
2024-08-19 02:24:16 +0500

    insta changes made by tayyab.

--- components/Feeds/Instagram.jsx
+++ components/Feeds/Instagram.jsx
@@ -13,26 +13,32 @@ import { jost } from "../ui/fonts";
 
 export default function Instagram() {
   return (
-    <Container>
-      <div className="flex flex-col w-full mt-10 overflow-hidden">
-        <h1 className={`mb-10 text-2xl xl:text-3xl font-semibold ${jost.className}`}>
-          SHOP OUR IG
-        </h1>
-        <div className="angry-grid">
-          {/* Grid of 4 pics */}
-          <Image src={ig_one} alt="IG One" id="item-0" />
-          <Image src={ig_three} alt="IG Three" id="item-1" />
-          <Image src={ig_two} alt="IG Two" id="item-2" />
-          <Image src={ig_four} alt="IG Four" id="item-3" />
+    <div className="flex w-full mb-10">
+      <Container>
+        <div className="flex flex-col w-full mt-10 overflow-hidden">
+          <h1
+            className={`mb-10 text-2xl xl:text-3xl font-semibold ${jost.className}`}
+          >
+            SHOP OUR IG
+          </h1>
+          <div className="ig-container">
+            <div className="angry-grid">
+              {/* Grid of 4 pics */}
+              <Image src={ig_one} alt="IG One" id="item-0" />
+              <Image src={ig_three} alt="IG Three" id="item-1" />
+              <Image src={ig_two} alt="IG Two" id="item-2" />
+              <Image src={ig_four} alt="IG Four" id="item-3" />
 
-          {/* Bigger grid */}
-          <Image src={ig_five} alt="IG Five" id="item-4" />
+              {/* Bigger grid */}
+              <Image src={ig_five} alt="IG Five" id="item-4" />
 
-          {/* Grid of 2 */}
-          <Image src={ig_six} alt="IG Six" id="item-5" />
-          <Image src={ig_seven} alt="IG Seven" id="item-6" />
+              {/* Grid of 2 */}
+              <Image src={ig_six} alt="IG Six" id="item-5" />
+              <Image src={ig_seven} alt="IG Seven" id="item-6" />
+            </div>
+          </div>
         </div>
-      </div>
-    </Container>
+      </Container>
+    </div>
   );
 }
