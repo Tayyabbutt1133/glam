39197bca4e30a07793713931bea86e3843f8eb52
Tayyeb Shahzad <tayyebbutt966@gmail.com>
2024-08-19 15:23:26 +0500

    category next arrow updated

--- components/home/home-products/shop_by_category/HomeCategory.jsx
+++ components/home/home-products/shop_by_category/HomeCategory.jsx
@@ -28,7 +28,7 @@ const NextArrow = ({ className, style, onClick }) => {
       style={{
         ...style,
         ...arrowStyles,
-        right: "10px",  // Adjusted to match left arrow
+        right: "2px",  // Adjusted to match left arrow
         display: "block",
       }}
     >
