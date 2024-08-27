2fda7a827fca2bab0d56d2c32ce370796312b776
Uzain Bin Mubarak <uzainmubarak@gmail.com>
2024-08-27 13:51:36 +0500

    Added className prop to container

--- components/container.jsx
+++ components/container.jsx
@@ -1,3 +1,3 @@
-export default function Container({ children }) {
-    return <div className="container w-[92%] mx-auto">{children}</div>;
+export default function Container({ children, className = "" }) {
+  return <div className={`container w-[92%] mx-auto ${className} `}>{children}</div>;
 }
