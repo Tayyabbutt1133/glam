112d988a5301a1767e1a5cc9c897e8bdf9926920
Uzain Bin Mubarak <uzainmubarak@gmail.com>
2024-08-28 12:29:33 +0500

    Update Aboutbrand.jsx

--- components/lifecycle/brand/Aboutbrand.jsx
+++ components/lifecycle/brand/Aboutbrand.jsx
@@ -13,16 +13,16 @@ export default function Aboutbrand() {
         </div>
         <div className='w-1/2 space-y-6 leading-relaxed'>
           <p className={`${lexendDeca.className} text-sm`}>
-            Achieve your perfect makeup look every time with the incredible range of products at GLAMBEAUTY. Featuring all your favourite iconic designer brands, {"you'll"} find high-quality <strong>eye makeup</strong>, lip makeup, complexion makeup, and the best accessories to suit every personality, look, and occasion. Whether you’re gearing up for a date night, bridal makeup for the big day, or simply your everyday essentials, explore our collection to find your go-to items.
+            Achieve your perfect makeup look every time with the incredible range of products at GLAMBEAUTY. Featuring all your favourite iconic designer brands, you&apos;ll find high-quality <strong>eye makeup</strong>, lip makeup, complexion makeup, and the best accessories to suit every personality, look, and occasion. Whether you’re gearing up for a date night, bridal makeup for the big day, or simply your everyday essentials, explore our collection to find your go-to items.
             <br />
             <br />
-            Discover <strong>foundations</strong> that work with your skin to tackle dryness, redness, and acne, along with <strong>concealers</strong> that match every skin type. Choose <strong>mascaras</strong> that lengthen, thicken, define, or enhance the health of your lashes. Opt for <strong>eyeshadows</strong> to add precision or create a smouldering smoky eye. Pick up lipsticks, liners, glosses, or stains to craft a natural look or achieve high drama – with such quality lip products, {"you'll"} enjoy a look that lasts all day.
+            Discover <strong>foundations</strong> that work with your skin to tackle dryness, redness, and acne, along with <strong>concealers</strong> that match every skin type. Choose <strong>mascaras</strong> that lengthen, thicken, define, or enhance the health of your lashes. Opt for <strong>eyeshadows</strong> to add precision or create a smouldering smoky eye. Pick up lipsticks, liners, glosses, or stains to craft a natural look or achieve high drama – with such quality lip products, you&apos;ll enjoy a look that lasts all day.
             <br />
             <br />
-            Our online shop offers all the shades, textures, and finishes to suit any skin tone and type. Dive in today to discover the best UK makeup brands alongside the most iconic lines from the {"world's"} top beauty names. Shop the range of cosmetics and makeup at GLAMBEAUTY today.
+            Our online shop offers all the shades, textures, and finishes to suit any skin tone and type. Dive in today to discover the best UK makeup brands alongside the most iconic lines from the world&apos;s top beauty names. Shop the range of cosmetics and makeup at GLAMBEAUTY today.
             <br />
             <br />
-            Dive in today to discover the best UK makeup brands alongside the most iconic lines from the {"world's"} top beauty names. Shop the range of cosmetics and makeup at GLAMBEAUTY today.
+            Dive in today to discover the best UK makeup brands alongside the most iconic lines from the world&apos;s top beauty names. Shop the range of cosmetics and makeup at GLAMBEAUTY today.
           </p>
           <div>
                       <button className={`w-36 ${jost.className} h-10 bg-black text-white rounded-md uppercase`}><a href="#">Shop now</a></button>
