5c12944750b0d05a3c824ca1120b108aa0bd3f7f
Uzain Bin Mubarak <uzainmubarak@gmail.com>
2024-09-01 04:33:39 +0500

    file structure created

--- src/app/brands/page.jsx
+++ src/app/brands/page.jsx
@@ -120,3 +120,5 @@ export default function Page() {
       </div>
   );
 }
+/brand/mac
+/product-categories/makeup
---
+++ src/app/product-categories/[landing-page]/page.jsx
@@ -0,0 +1,3 @@
+export default function LandingPage({params}){
+  return <div>slug: {params['landing-page']}</div>
+}
