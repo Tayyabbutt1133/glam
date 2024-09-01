636b63bd32e0850d23efb3649e0c8ed3d94c7b48
Uzain Bin Mubarak <uzainmubarak@gmail.com>
2024-09-01 23:22:23 +0500

    Update navigation.jsx

--- components/header/navigation-nav/navigation.jsx
+++ components/header/navigation-nav/navigation.jsx
@@ -3,33 +3,14 @@ import { useState, useEffect } from "react";
 import Link from "next/link";
 import { Jost } from "next/font/google";
 import Container from "../../container";
-import MegaMenu from "./megamenu"; // Make sure the path is correct
-import axios from "axios";
+import MegaMenu from "./megamenu";
 
 const jost = Jost({ subsets: ["latin"] });
 
-// let links = [
-//   { name: "Sale", link: "/sale" },
-//   { name: "New In", link: "/new-in" },
-//   { name: "Brands", link: "/brands" },
-//   { name: "Makeup", link: "/product-categories/makeup" },
-//   { name: "Skincare", link: "/product-categories/skincare" },
-//   { name: "Hair", link: "/product-categories/hair" },
-//   { name: "Nails", link: "/product-categories/nails" },
-//   { name: "Fragrance", link: "/product-categories/fragrance" },
-//   { name: "Bath & Body", link: "/product-categories/bath-&-body" },
-//   { name: "Home", link: "/product-categories/homes" },
-//   { name: "Wellness", link: "/product-categories/wellness" },
-//   { name: "Electrics", link: "/product-categories/electrics" },
-// ];
-
-
 export default function Navigation() {
-  const [hoveredLink, setHoveredLink] = useState(null);
+  const [hoveredLink, setHoveredLink] = useState({ id: null, href: null});
   const [links, setLinks] = useState([]);
   const mainLinks = links?.filter(link => link.parent === "0") 
-  
-  
   
   useEffect(()=> {
     const fetchLinks = async () => {
@@ -51,17 +32,19 @@ export default function Navigation() {
   
   // no megamenu for sale and new-in
   const shouldShowMegaMenu = links?.find(link => {
-    return link.id === hoveredLink && link.href !== "/sale" && link.href !== "/new-in"
+    return link.id === hoveredLink.id && link.href !== "/sale" && link.href !== "/new-in"
   })
 
   const handleLinkClick = () => {
-    setHoveredLink(null);
+    setHoveredLink(
+      { id: null, href: null}
+    );
   };
 
   return (
     <div
       className={`${jost.className} hidden lg:flex items-center h-[52px] font-normal lg:text-base xl:text-sm bg-white text-sm 2xl:text-lg relative`}
-      onMouseLeave={() => setHoveredLink(null)} // Close mega menu when mouse leaves the entire navigation area
+      onMouseLeave={() => setHoveredLink({ id: null, href: null})} // Close mega menu when mouse leaves the entire navigation area
     >
       <Container>
         <nav className="flex flex-row w-full justify-between items-center py-3">
@@ -73,10 +56,10 @@ export default function Navigation() {
               key={link.id}
               href={link.href}
               style={{
-                boxShadow: hoveredLink === link.id ? `inset 0 -2px 0 0 var(--color-hover)` : "inset 0 -2px 0 0 transparent",
+                boxShadow: hoveredLink.id === link.id ? `inset 0 -2px 0 0 var(--color-hover)` : "inset 0 -2px 0 0 transparent",
                 transition: "box-shadow 0.3s ease",
               }}
-              onMouseEnter={() => setHoveredLink(link.id)}
+              onMouseEnter={() => setHoveredLink({ id: link.id, href: link.href})}
               onClick={handleLinkClick}
             >
               <div>{link.name}</div>
