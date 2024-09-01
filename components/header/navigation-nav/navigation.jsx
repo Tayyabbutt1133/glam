3cc3d9d9d3c62174e3ca7790c3a01c2d90046dad
Uzain Bin Mubarak <uzainmubarak@gmail.com>
2024-09-01 19:02:56 +0500

    axios get method removed and fetch used

--- components/header/navigation-nav/navigation.jsx
+++ components/header/navigation-nav/navigation.jsx
@@ -34,7 +34,7 @@ export default function Navigation() {
   useEffect(()=> {
     const fetchLinks = async () => {
       try{
-        const res = await axios.get('/api/nav')
+        const res = await fetch('/api/nav', {next: {revalidate: 60}})
         const data = res.data
         setLinks(data)
       }
--- src/app/api/nav/route.js
+++ src/app/api/nav/route.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 export async function GET(request) {
-  const res = await axios.get(`https://glam.clickable.site/wp-json/wp/v2/test-6`);
+  const res = await fetch(`https://glam.clickable.site/wp-json/wp/v2/test-6`, {
+    method: "GET",
+    next: { revalidate: 60 },
+  });
   const data = res.data;
   return Response.json(data);
 }
