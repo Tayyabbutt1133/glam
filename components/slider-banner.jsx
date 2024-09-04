3a85a29a6747f786d206e0ee222d23976297e072
Uzain Bin Mubarak <uzainmubarak@gmail.com>
2024-09-04 22:48:38 +0500

    hero slider fixed changes

--- components/slider-banner.jsx
+++ components/slider-banner.jsx
@@ -2,17 +2,20 @@ import Image from "next/image";
 import Text from "./ui/Text";
 import Button from "./ui/button";
 
-export default function SliderBanner({ bannerObject }) {
+export default function SliderBanner({ bannerObject, hero = false }) {
   const { title, description, src } = bannerObject;
 
   return (
     <div className="w-full flex h-full lg:max-h-[693px]">
       {/* Left side of slider */}
       <div className="w-[35%] flex flex-col bg-bg-01 justify-center items-center">
-        <div className="flex flex-col w-[60%] gap-10 items-start ">
-          <Text style={"h1"} className="uppercase">
+        <div className={`flex flex-col w-[60%] gap-10 ${hero ? "items-center text-center" : "items-start" }`}>
+          <Text 
+            style={"h1"} 
+            className={`uppercase hero-title`}>
             {title}
           </Text>
+
           <Text style={"sm"} className="text-light capitalize">
             {description}
           </Text>
