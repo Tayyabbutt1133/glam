8733e712f8bb6adffe46fbcba83d958230731cf4
Tayyeb Shahzad <tayyebbutt966@gmail.com>
2024-09-09 13:41:53 +0500

    my cart font updated

--- components/header/middle-bar-nav/middle-bar-nav.jsx
+++ components/header/middle-bar-nav/middle-bar-nav.jsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { useCartStore } from '../../../states/Cardstore'; // Adjust the path
 import { CartIcon } from '../../../public/icons/cart'; // Make sure this path is correct
 import CartDropdown from '../../Cartdropdown'; // Adjust the path
@@ -8,13 +8,48 @@ import Link from 'next/link';
 import SearchBarWithDropdown from './searchbar'
 import glam_logo from '../../../public/logo.svg'
 import Image from 'next/image';
+import { UserIcon } from '../../../public/icons/user';
+import { jost } from '../../ui/fonts';
+
+
 
 export default function MiddleBarNav() {
+
+
+
+
+
+
+
+
+
+
   const [isCartOpen, setIsCartOpen] = useState(false);
   const cartItems = useCartStore((state) => state.cartItems);
 
   const toggleCartDropdown = () => {
     setIsCartOpen((prev) => !prev);
+  };
+
+
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  // const dropdownRef = useRef(null);
+  const accountLinkRef = useRef(null);
+
+  const handleMouseEnterAccount = () => {
+    setIsDropdownOpen(true);
+  };
+
+  const handleMouseLeaveAccount = () => {
+    setIsDropdownOpen(false);
+  };
+
+  const handleMouseEnterDropdown = () => {
+    setIsDropdownOpen(true);
+  };
+
+  const handleMouseLeaveDropdown = () => {
+    setIsDropdownOpen(false);
   };
 
   return (
@@ -37,8 +72,20 @@ export default function MiddleBarNav() {
           {/* Account and Cart */}
           <section className="flex flex-row justify-between items-center gap-6 text-primary">
             {/* Account Section */}
-            {/*...*/}
-
+            <div className="relative">
+                <div
+                  ref={accountLinkRef}
+                  onMouseEnter={handleMouseEnterAccount}
+                  onMouseLeave={handleMouseLeaveAccount}
+                >
+                  <Link href="./signup">
+                    <div className="flex flex-row justify-center items-center gap-3 ">
+                      <UserIcon className={"w-4"} />
+                      <span className={`capitalize text-nowrap font-normal lg:text-base xl:text-lg ${jost.className}`}>my account</span>
+                    </div>
+                  </Link>
+              </div>
+              </div>
             {/* Cart Section */}
             <div className="relative">
   <div
@@ -53,7 +100,7 @@ export default function MiddleBarNav() {
         </span>
       )}
     </div>
-    <span className="capitalize text-nowrap lg:text-base font-normal xl:text-lg">
+                <span className={`capitalize ${jost.className} text-nowrap lg:text-base font-normal xl:text-lg`}>
       My Bag
     </span>
   </div>
