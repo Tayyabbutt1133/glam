b700f1272e91c054150e6ed771a86c472f93de30
Uzain Bin Mubarak <uzainmubarak@gmail.com>
2024-09-13 14:53:34 +0500

    changes

---
+++ src/app/product/[productSlug]/page.jsx
@@ -0,0 +1,10 @@
+import Container from "@/components/container";
+import SingleProductData from "@/components/single-product/single-product-data";
+
+export default function Page() {
+  return (
+    <Container className="my-20">
+      <SingleProductData />
+    </Container>
+  );
+}
