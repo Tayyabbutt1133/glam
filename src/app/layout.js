c07bea16129cac24f707eefa0215f85e5ea1eb62
Tayyeb Shahzad <tayyebbutt966@gmail.com>
2024-09-19 21:32:09 +0500

    analytics added

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,18 +6,21 @@ import Newsletter from "../../components/Newsletter";
 import Footer from "../../components/Footer";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-const inter = Inter({ subsets: ["latin"] });
+import Head from 'next/head';
 
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Glam Studio",
   description: "Home page glam studio",
 };
 
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+      <script type="text/javascript" src="https://www.bugherd.com/sidebarv2.js?apikey=fhpij2lzixqjqywxhkpnba" async="true"></script>
+      </Head>
       <body className={inter.className}>
         <CurrencyLanguagePopUp />
         <Header/>
@@ -26,6 +29,6 @@ export default function RootLayout({ children }) {
         <Newsletter />
         <Footer/>
       </body>
-      </html>
+    </html>
   );
 }
