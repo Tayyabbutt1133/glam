40eec6db3e4bba7d919c880cc05e8768b84b3b50
Uzain Bin Mubarak <uzainmubarak@gmail.com>
2024-09-20 18:30:55 +0500

    megamenu close on link click

--- components/header/navigation-nav/megamenu.jsx
+++ components/header/navigation-nav/megamenu.jsx
@@ -6,12 +6,14 @@ import Text from "../../ui/Text";
 import BrandMegaMenu from "./brands-megamenu";
 import img from "/public/nav-images/skincare-nav.png";
 
-
-
-
-export default function MegaMenu({ links, hoveredLink }) {
+export default function MegaMenu({ links, hoveredLink, closeMegaMenu }) {
   const getSubMenu = (parentId) => {
     return links.filter((link) => link.parent === parentId?.toString());
+  };
+
+  const handleLinkClick = (e) => {
+    // Allow the default link behavior
+    closeMegaMenu();
   };
 
   return (
@@ -19,41 +21,39 @@ export default function MegaMenu({ links, hoveredLink }) {
       className="relative w-full bg-white shadow-lg z-50 transition duration-300 ease-in-out"
       style={{ zIndex: "9" }}
     >
-      {/* Horizontal line */}
-      <div className=" w-full border border-b-02"></div>
+      <div className="w-full border border-b-02"></div>
       {hoveredLink.href === "/brands" ? (
-        <BrandMegaMenu links={links} />
+        <BrandMegaMenu closeMegaMenu={closeMegaMenu} />
       ) : (
         <section className="flex flex-row w-full h-full max-h-[386px]">
-          {/* Submenu container */}
           <div className="w-[70%] flex justify-center py-5 px-[2%]">
             <div className="flex flex-row w-full justify-start gap-4 md:gap-6 lg:gap-8 overflow-x-auto overflow-y-hidden">
               {getSubMenu(hoveredLink.id).length > 0 &&
                 getSubMenu(hoveredLink.id).map((link, index) => (
                   <div key={index} className="min-w-fit mr-0">
-                    {/* Submenu title */}
-                    {/* change the href link to id  */}
-                    <Link href={link.href =="/product-categories/skin-care/Moisturizers"?"/product-categories/skin-care/Moisturisers":link.href}>
-                      <Text style={"h4"} className={`uppercase mb-5`}>
+                    <Link 
+                      href={link.href === "/product-categories/skin-care/Moisturizers" ? "/product-categories/skin-care/Moisturisers" : link.href}
+                      onClick={handleLinkClick}
+                    >
+                      <Text style="h4" className="uppercase mb-5">
                         {link.name}
                       </Text>
                     </Link>
-                    {/* Submenu items */}
                     <ul className="flex flex-wrap flex-col text-sm h-[300px] gap-4">
-                      {getSubMenu(hoveredLink.id).length > 0 &&
-                        getSubMenu(link.id).map((subLink, index) => (
-                          <Link key={index} href={subLink.href}>
+                      {getSubMenu(link.id).length > 0 &&
+                        getSubMenu(link.id).map((subLink, subIndex) => (
+                          <Link key={subIndex} href={subLink.href} onClick={handleLinkClick}>
                             <li
                               className={`flex flex-row items-center cursor-pointer ${lexendDeca.className} mr-5`}
                             >
-                              {subLink.color ? (
+                              {subLink.color && (
                                 <div
                                   style={{
                                     backgroundColor: subLink.color,
                                   }}
-                                  className={`w-4 h-4 rounded-full mr-2`}
+                                  className="w-4 h-4 rounded-full mr-2"
                                 ></div>
-                              ) : null}
+                              )}
                               {subLink.name}
                             </li>
                           </Link>
@@ -62,9 +62,7 @@ export default function MegaMenu({ links, hoveredLink }) {
                   </div>
                 ))}
             </div>
-          </div>{" "}
-          {/* Submenu container */}
-          {/* Image */}
+          </div>
           <div className="grid w-[30%] h-auto justify-end">
             <Image
               src={hoveredLink.img || img}
