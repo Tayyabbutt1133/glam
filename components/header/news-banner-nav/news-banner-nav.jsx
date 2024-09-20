f78b51a935d5019a99a5a1720243b87e809b1aca
Tayyeb Shahzad <tayyebbutt966@gmail.com>
2024-09-20 15:10:45 +0500

    news popup updated

--- components/header/news-banner-nav/news-banner-nav.jsx
+++ components/header/news-banner-nav/news-banner-nav.jsx
@@ -1,88 +1,68 @@
-"use client";
-import { useEffect, useState } from "react";
-import Image from "next/image";
-import ArrowDown from "../../../public/icons/arrow-down";
-// import { Lexend_Deca } from "next/font/google";
-import { lexendDeca } from "../../ui/fonts";
+'use client'
 
-import Container from "../../container";
-import { usePopupStore } from "../../../states/use-popup-store.jsx";
-
-// const lexendDeca = Lexend_Deca({ subsets: ["latin"] });
+import { useEffect, useState } from "react"
+import Image from "next/image"
+import ArrowDown from "../../../public/icons/arrow-down"
+import { lexendDeca } from "../../ui/fonts"
+import Container from "../../container"
+import { usePopupStore } from "../../../states/use-popup-store.jsx"
 
 export default function NewsBannerNav() {
-  const [mounted, setMounted] = useState(false);
-  const onOpen = usePopupStore((state) => state.onOpen);
-  const selectedCountryFromStore = usePopupStore(
-    (state) => state.selectedCountry
-  );
-  // const {rate,currencySymbol} = usePopupStore()
+  const [mounted, setMounted] = useState(false)
+  const onOpen = usePopupStore((state) => state.onOpen)
+  const selectedCountryFromStore = usePopupStore((state) => state.selectedCountry)
+
+  const defaultCountry = { countryCode: "GB", code: "GBP", country: "United Kingdom" }
 
   const [selectedCountry, setSelectedCountry] = useState(() => {
-    // Check if window is defined to ensure code runs in the browser
     if (typeof window !== "undefined") {
-      if (selectedCountryFromStore && selectedCountryFromStore.countryCode) {
-        return selectedCountryFromStore;
-      }
-      const savedCountry = localStorage.getItem("selectedCountry");
+      const savedCountry = localStorage.getItem("selectedCountry")
       return savedCountry
         ? JSON.parse(savedCountry)
-        : { countryCode: "", code: "", country: "" };
+        : defaultCountry
     } else {
-      return { countryCode: "", code: "", country: "" }; // Default value during SSR
+      return defaultCountry
     }
-  });
+  })
 
-  const [flagUrl, setFlagUrl] = useState("");
+  const [flagUrl, setFlagUrl] = useState("")
 
   const fetchFlag = async (countryCode) => {
     try {
-      const response = await fetch(
-        `https://restcountries.com/v3.1/alpha/${countryCode}`
-      );
-      const data = await response.json();
-      setFlagUrl(data[0].flags.svg);
+      const response = await fetch(`https://restcountries.com/v3.1/alpha/${countryCode}`)
+      const data = await response.json()
+      setFlagUrl(data[0].flags.svg)
     } catch (error) {
-      console.error("Error fetching flag:", error);
+      console.error("Error fetching flag:", error)
     }
-  };
+  }
 
   useEffect(() => {
     if (selectedCountry.countryCode) {
-      fetchFlag(selectedCountry.countryCode);
+      fetchFlag(selectedCountry.countryCode)
     }
-  }, [selectedCountry]);
+  }, [selectedCountry])
 
   useEffect(() => {
-    // Sync state with the store when the popup selection changes
     const unsubscribe = usePopupStore.subscribe((state) => {
-      const updatedCountry = state.selectedCountry;
-      if (
-        updatedCountry &&
-        updatedCountry.countryCode !== selectedCountry.countryCode
-      ) {
-        setSelectedCountry(updatedCountry);
+      const updatedCountry = state.selectedCountry
+      if (updatedCountry && updatedCountry.countryCode !== selectedCountry.countryCode) {
+        setSelectedCountry(updatedCountry)
         if (typeof window !== "undefined") {
-          localStorage.setItem(
-            "selectedCountry",
-            JSON.stringify(updatedCountry)
-          );
+          localStorage.setItem("selectedCountry", JSON.stringify(updatedCountry))
         }
       }
-    });
+    })
 
-    return () => unsubscribe();
-  }, [selectedCountry]);
+    return () => unsubscribe()
+  }, [selectedCountry])
 
-  // Set mounted to true after the component mounts
-  // This is to prevent the component from rendering on the server
-  // and causing a hydration mismatch
   useEffect(() => {
-    setMounted(true);
-  }, []);
+    setMounted(true)
+  }, [])
 
   if (!mounted) {
-    return null;
+    return null
   }
 
   return (
@@ -105,21 +85,17 @@ export default function NewsBannerNav() {
               )}
             </div>
             <button
-              className={`flex flex-row items-center cursor-pointer gap-2 `}
+              className={`flex flex-row items-center cursor-pointer gap-2`}
               onClick={onOpen}
             >
-              <div className={`font-normal text-base `}>
-                    <span className="lowercase">
-                        {selectedCountry.countryCode}
-                    </span>
-                     {" "} - {selectedCountry.code}
-                </div>
-              
+              <div className={`font-normal text-base`}>
+                <span className="lowercase">{selectedCountry.countryCode}</span> - {selectedCountry.code}
+              </div>
               <ArrowDown className="w-3 h-auto" />
             </button>
           </div>
         </div>
       </Container>
     </div>
-  );
+  )
 }
