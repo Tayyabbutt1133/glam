b84a780b4a81a69c978495fbf9a48107135b2542
Tayyeb Shahzad <tayyebbutt966@gmail.com>
2024-09-22 14:29:26 +0500

    issue 20 resolved

--- components/home/home-products/shop_by_brand/HomeBrand.jsx
+++ components/home/home-products/shop_by_brand/HomeBrand.jsx
@@ -80,12 +80,12 @@ const HomeBrand = () => {
 
               <div className=" py-6 text-left">
                 <h3
-                  className={`hidden sm:block text-lg font-semibold text-gray-900 ${jost.className}`}
+                  className={`hidden  sm:block text-lg font-semibold text-gray-900 ${jost.className}`}
                 >
                   {product.name}
                 </h3>
                 <p
-                  className={`text-black text-xs sm:text-sm my-2 ${lexendDeca.className}`}
+                  className={`text-black font-normal text-xs sm:text-sm my-2 ${lexendDeca.className}`}
                 >
                   {product.description}
                 </p>
