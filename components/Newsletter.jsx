fd9993ce60d72136086621818c5bb2144df7cff3
Uzain Bin Mubarak <uzainmubarak@gmail.com>
2024-09-29 21:37:20 +0500

    newsletter button

--- components/Newsletter.jsx
+++ components/Newsletter.jsx
@@ -40,7 +40,7 @@ export default function Newsletter() {
                
                     <button
                       type="submit"
-                      className={`box-border hover:bg-[#CF8562]  h-[36px] md:h-[46px]  2xl:h-[55px]  px-7 uppercase   text-sm lg:text-[17px] 2xl:text-[20px] font-medium text-center text-white rounded-lg border cursor-pointer bg-primary-700 border-primary-600 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 bg-black ${jost.className}`}
+                      className={`box-border hover:bg-hover px-7 uppercase   text-sm lg:text-[17px] 2xl:text-[20px] font-medium text-center text-white rounded-lg border cursor-pointer bg-primary-700 border-primary-600 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 bg-black ${jost.className}`}
                     >
                       Subscribe
                     </button>
