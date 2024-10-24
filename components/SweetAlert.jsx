1d68e1c4532b6c8a63b46885d6d15b1a6aa49d8c
Tayyeb Shahzad <tayyebbutt966@gmail.com>
2024-10-24 09:48:23 +0500

    code push

---
+++ components/SweetAlert.jsx
@@ -0,0 +1,19 @@
+"use client"
+import React, { useEffect } from 'react';
+import Swal from 'sweetalert2'; // Correctly importing SweetAlert2
+
+const SaleAlert = () => {
+  useEffect(() => {
+    // Trigger SweetAlert when the component is mounted
+    Swal.fire({
+      title: "This Website is for Sale!",
+      text: "If you're interested, contact me at: tayyebbutt966@gmail.com",
+      icon: "info",
+      confirmButtonText: "Close"
+    });
+  }, []);
+
+  return null; // No need to return JSX
+};
+
+export default SaleAlert;
