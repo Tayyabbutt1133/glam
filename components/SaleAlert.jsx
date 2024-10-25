12e54eee1541f6ccffcfab93b1fae84640cbd2a6
Tayyeb Shahzad <tayyebbutt966@gmail.com>
2024-10-25 08:09:11 +0500

    sale alert updated

--- components/SaleAlert.jsx
+++ components/SaleAlert.jsx
@@ -6,7 +6,8 @@ const SaleAlert = () => {
   useEffect(() => {
     // Trigger SweetAlert when the component is mounted
     Swal.fire({
-      title: "This Website is for Sale!",
+      title: "This Website is for Sale Now!",
+      text: "Website Development has been stopped because client does'nt pay developer",
       text: "If you're interested, contact me at: tayyebbutt966@gmail.com",
       icon: "info",
       confirmButtonText: "Close"
